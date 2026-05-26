ca98a72d0e714c4c85a0b23604e72ef4565b1b9f
SpacemanOctopus <kristian.leth@hotmail.dk>
2026-05-26 14:06:21 +0200

    added video, made navbar and footer into javascript files

---
+++ js/footer.js
@@ -0,0 +1,31 @@
+document.getElementById("footer").innerHTML = `
+<div class="pt-3 pb-5 position-relative container-fluid bottomfooter">
+
+    <p class="text-white">
+        Kontakt os på <br>
+        Tlf: +12 3456 7890 <br>
+        Email: sejero@sejero.dk
+    </p>
+
+    <p class="pb-3 text-white">
+        Find os på <br>
+
+        <i class="fa-brands fa-facebook"></i>
+        @sejerodk
+        <br>
+
+        <i class="fa-brands fa-square-instagram"></i>
+        @sejerodk
+    </p>
+
+    <a target="_blank" href="https://danske-smaaoer.dk/">
+
+        <img
+            class="øsammenslutningen position-absolute"
+            src="img/Ø-sammenslutningenlogo.png"
+            alt="Ø-sammenslutningen logo">
+
+    </a>
+
+</div>
+`;
---
+++ js/navbar.js
@@ -0,0 +1,49 @@
+document.getElementById("navbar").innerHTML = `
+<nav class="navbar navbar-expand-md">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="Forside.html">
+			<img class="headerlogo" src="img/Sejerø logo - Hvid.webp" alt="Sejerøs logo">
+		</a>
+
+		<button class="navbar-toggler" type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarNav"
+			aria-controls="navbarNav"
+			aria-expanded="false"
+			aria-label="Toggle navigation">
+
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+
+				<li class="nav-item">
+					<a class="nav-link barlink" href="Havet.html">Havet</a>
+				</li>
+
+				<li class="nav-item">
+					<a class="nav-link barlink" href="Økultur.html">Økultur</a>
+				</li>
+
+				<li class="nav-item">
+					<a class="nav-link barlink" href="Bolig.html">Bolig</a>
+				</li>
+
+				<li class="nav-item">
+					<a class="nav-link barlink" href="Om Sejerø.html">Om Sejerø</a>
+				</li>
+
+				<li class="nav-item ferrylink align-self-end ps-1 pe-1 rounded-3">
+					<a target="_blank"
+					   class="nav-link"
+					   href="https://sejeroe-ferry.teambooking.dk/new-booking">
+					   Færge Booking
+					</a>
+				</li>
+
+			</ul>
+		</div>
+	</div>
+</nav>
+`;
