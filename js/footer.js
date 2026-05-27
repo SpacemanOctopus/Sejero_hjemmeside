37cd625d8c626b3989fccd9370350495d87b8e48
SpacemanOctopus <kristian.leth@hotmail.dk>
2026-05-27 14:00:38 +0200

    almost finished with landing, begun on "havet".

--- js/footer.js
+++ js/footer.js
@@ -1,27 +1,27 @@
 document.getElementById("footer").innerHTML = `
-<div class="pt-3 pb-5 position-relative container-fluid bottomfooter">
+<div class="pt-3 pb-5 container-fluid bottomfooter">
 
     <p class="text-white">
         Kontakt os på <br>
         Tlf: +12 3456 7890 <br>
-        Email: sejero@sejero.dk
+        <a class="link-light" href="mailto:sejero@sejero.dk">Email: sejero@sejero.dk</a>
     </p>
 
     <p class="pb-3 text-white">
         Find os på <br>
 
-        <i class="fa-brands fa-facebook"></i>
-        @sejerodk
+        <a class="link-light" target="_blank" href="https://www.facebook.com/Sejerodk/"><i class="fa-brands fa-facebook"></i>
+        @sejerodk</a>
         <br>
 
-        <i class="fa-brands fa-square-instagram"></i>
-        @sejerodk
+        <a class="link-light" target="_blank" href="https://www.instagram.com/"><i class="fa-brands fa-square-instagram"></i>
+        @sejerodk</a>
     </p>
 
     <a target="_blank" href="https://danske-smaaoer.dk/">
 
         <img
-            class="øsammenslutningen position-absolute"
+            class="øsammenslutningen"
             src="img/Ø-sammenslutningenlogo.png"
             alt="Ø-sammenslutningen logo">
 
